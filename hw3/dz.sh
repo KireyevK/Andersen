a3544d70135e4bc3fa9524b7ebe5d8da09537ded
KireyevK <94606941+KireyevK@users.noreply.github.com>
2021-11-26 17:08:29 +0200

    shellcheck

--- hw3/dz.sh
+++ hw3/dz.sh
@@ -7,10 +7,10 @@ get_dest_addr(){ :
 }
 
 read_org(){
-	while read -r IP; do
-		whois $IP | grep "^$whois_str" | tr  -s ' ' | cut -d' ' -f2- 
-	done
-}	
+        while read -r IP; do
+                whois "$IP" | grep "^$whois_str" | tr  -s ' ' | cut -d' ' -f2- 
+        done
+}
 
 get_list(){
     echo "run $0 whith arg name or pid"
@@ -21,35 +21,35 @@ get_list(){
 }
 
 check_get_name(){
-    ps -C $1 -o comm= 2> /dev/null && return 0
-    ps -p $1 -o comm= 2> /dev/null && return 0
+    ps -C "$1" -o comm= 2> /dev/null && return 0
+    ps -p "$1" -o comm= 2> /dev/null && return 0
     return 1 
 }
 
 check_requrements(){ 
     declare -i status=0
     for i ; {
-        command -v $i &> /dev/null \
+        command -v "$i" &> /dev/null \
             && err_str+="$i \033[36mFound\033[0m\n" \
             || { 
                 err_str+="$i \033[31mNot Found\033[0m\n" 
                 status=1
                 }
         }
-    (( $status == 0 )) || echo -e "$err_str" 
+    (( status == 0 )) || echo -e "$err_str" 
     return $status
 }
 
-check_requrements "ss sed sort ps whois grep tr cut" || get_list
-(( $EUID == 0 )) || echo "non-owned process info will not be shown, you would have to be root to see it all.)"
-(( $# == 1 )) || echo "run whith 1 args" && get_list 
-COMM="$(check_get_name $1 | head -1)" 
+check_requrements ss sed sort ps whois grep tr cut || get_list
+(( EUID == 0 )) || echo "non-owned process info will not be shown, you would have to be root to see it all.)"
+(( $# == 1 )) || { echo "run whith 1 args"; get_list;} 
+COMM="$(check_get_name "$1" | head -1)" 
 [[ -z "$COMM" ]] && echo "command or pid - incorrect" && get_list
 
-declare -i num_lines=0
+declare -i num_lines=5
 whois_str=Organization
 
-< <(get_dest_addr $name) read_org | sort | uniq -c | tail -$num_lines
+< <(get_dest_addr) read_org | sort | uniq -c | tail -$num_lines
 
 
 
